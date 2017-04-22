7b831ab6745a2b86485b8d2ad51269de5ec34647
Jakub Wietecha <estissy@gmail.com>
2017-04-22 22:01:44 +0200

    Zmiany w strukurze kontrolerów

--- Server/routes.js
+++ Server/routes.js
@@ -1,4 +1,7 @@
-var userController = require('./controllers/userController.js')
+var register = require('./controllers/user/register.js')
+var confirmRegistration = require('./controllers/user/confirmRegistration.js')
+var checkEmail = require('./controllers/user/checkEmail.js')
+
 
 module.exports = function(app) {
 
@@ -6,5 +9,12 @@ module.exports = function(app) {
     response.send('Hello world from server!')
   })
 
-  app.post('/api/v1/users/register', userController.register)
+  app.post('/api/v1/users/register', register.main)
+
+  app.use('/api/v1/users/:userId/register/confirm/:tokenId/:token', confirmRegistration.validUserMiddleware)
+  app.use('/api/v1/users/:userId/register/confirm/:tokenId/:token', confirmRegistration.validTokenMiddleware)
+  app.get('/api/v1/users/:userId/register/confirm/:tokenId/:token', confirmRegistration.main)
+
+  app.use('/api/v1/users/checkemail', checkEmail.validEmail)
+  app.post('/api/v1/users/checkemail', checkEmail.main)
 }
