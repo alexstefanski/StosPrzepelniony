88c7cd8356d0ba8683b99100c67da273b17bf6ad
Bartosz Sowa <b.sowa9@gmail.com>
2017-04-29 20:18:12 +0200

    Dodano model: Permission

---
+++ Server/models/Permission.js
@@ -0,0 +1,21 @@
+/*
+ * Created by sowka on 29/04/17
+ */
+
+ var Sequelize = require('sequelize');
+ var sequelize = require('./../config/sequelize.js');
+
+ var Permission = sequelize.define('permission',
+     {
+        name: {
+          type: Sequelize.STRING,
+          validate: {
+              isNull: {
+                msg: "Nazwa uprawnienia nie może być pusta!"
+              }
+          }
+        }
+
+ });
+
+module.exports = Permission;
