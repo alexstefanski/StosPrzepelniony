8679d52d41afa9c77a1f4434bf1a9e64ffaac263
Jakub Wietecha <estissy@gmail.com>
2017-05-01 13:43:52 +0200

    Poprawki wizualne.

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var Sequelize = require('sequelize');
 var bodyParser = require('body-parser');
 
-
 var app = express();
 app.use(bodyParser.urlencoded({
     extended: true
@@ -11,8 +10,6 @@ app.use(bodyParser.json());
 var myurl = express.Router();
 require('./routes.js')(myurl);
 app.use('/', myurl);
-
-var user = require('./models/User.js');
 
 app.listen(3000);
 console.log('Server started at port 3000 ...');
