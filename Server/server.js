12f0dbdf3013d82c81a240cbd8e81f0e3c33f6ff
Jakub Wietecha <estissy@gmail.com>
2017-05-02 13:37:46 +0200

    Uproszczenie kodu.

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,11 @@
-var express = require('express');
-var Sequelize = require('sequelize');
-var bodyParser = require('body-parser');
+var express = require('express')
+var Sequelize = require('sequelize')
+var bodyParser = require('body-parser')
+var routes = require('./routes.js')
 
-var app = express();
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-var myurl = express.Router();
-require('./routes.js')(myurl);
-app.use('/', myurl);
+var app = express()
+app.use(bodyParser.json())
+routes(app)
 
-app.listen(3000);
-console.log('Server started at port 3000 ...');
+app.listen(3000)
+console.log('Server started at port 3000 ...')
