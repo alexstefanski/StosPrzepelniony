565d9171478dead0ffe37b81f45ec8921915fb6f
Jakub Wietecha <estissy@gmail.com>
2017-05-09 14:59:53 +0200

    Poprawki wizualne. Dodano opis działania.

--- Server/middlewares/authenticatedUser.js
+++ Server/middlewares/authenticatedUser.js
@@ -4,6 +4,12 @@ var UserToken = require('./../models/index.js').UserToken
 var auth = require('basic-auth')
 var moment = require('moment')
 
+/**
+ * Middleware sprawdza czy żądanie zawiera nagłówek: Authorization typu Basic
+ * wraz z danymi. Następnie weryfikuje ważność danych (userId, sessionToken):
+ * 1. Sprawdza czy sessionToken należy do użytkownika.
+ * 2. Sprawdza czy sessionToken jest jeszcze ważny.
+ */
 module.exports.main = function(request, response, next) {
 
   var authData = auth(request)
@@ -14,19 +20,19 @@ module.exports.main = function(request, response, next) {
     UserToken.findOne({
       where: {
         token: authData.pass,
-        $and: [
-          { status: 10, userId: authData.name }
-        ]
+        $and: [{
+          status: 10,
+          userId: authData.name
+        }]
       }
-    })
-    .then(function(userToken) {
-      if(userToken != null) {
+    }).then(function(userToken) {
+      if (userToken != null) {
 
         var tokenExpiresIn = 31
         var createdMoment = moment(userToken.createdAt, "YYYY-MM-DD HH:mm:ss:SSS ZZ")
 
         // Check if token is live.
-        if(createdMoment.add(tokenExpiresIn, 'days') > moment()) {
+        if (createdMoment.add(tokenExpiresIn, 'days') > moment()) {
           next()
         } else {
           response.status(408).json()
