543a21fa06e565c4e11e5b09a8ae7f65ac7e2f0b
Jakub Wietecha <estissy@gmail.com>
2017-05-09 15:01:16 +0200

    Dodano tworzenie stałego użytkownika, administratora.

--- Server/seeders/userSeeder.js
+++ Server/seeders/userSeeder.js
@@ -1,5 +1,21 @@
 var User = require('./../models/user.js')
+var Admin = require('./../models/admin.js')
 
+// User for admin
+User.create({
+  email: 'admin1@localhost.com',
+  firstName: 'Kot',
+  lastName: 'Marek',
+  password: 'qwerty',
+  status: 1
+}).then(function(user) {
+  Admin.create({
+    userId: user.id,
+    permissionId: 1
+  })
+})
+
+// Extra random users
 var first_names = ['Maria', 'Krystyna', 'Anna', 'Barbara', 'Teresa', 'Elżbieta', 'Janina', 'Zofia', 'Jadwiga', 'Danuta', 'Halina', 'Irena', 'Ewa', 'Małgorzata', 'Helena', 'Grażyna', 'Bożena', 'Stanisława', 'Jolanta'];
 
 var last_names = ['Nowak', 'Kowalska', 'Wiśniewska', 'Dąbrowska', 'Lewandowska', 'Wójcik', 'Kamińska', 'Kowalczyk', 'Zielińska', 'Szymańska', 'Woźniak', 'Kozłowska', 'Jankowska', 'Wojciechowska'];
