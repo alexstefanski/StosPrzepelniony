731556ee1283084369899e72de87294c9fae32ed
Lucroth <lewandowskyyy@gmail.com>
2017-05-28 23:52:38 +0200

    Zmiana nazw komponentów

---
+++ Web application/src/app/admin/permission/index/admin-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './admin-permission-index.component.html',
+  styleUrls: ['./admin-permission-index.component.css']
+})
+export class AdminIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
