b040dbf3898415053a6f56ede9a34699e16f593f
Jakub Wietecha <estissy@gmail.com>
2017-06-07 23:50:23 +0200

    Dodanie tworzenia przykładowych kategorii.

---
+++ Server/seeders/categorySeeder.js
@@ -0,0 +1,18 @@
+var Category = require('./../models/category.js')
+
+Category.bulkCreate([
+  {name: 'Android', categoryIdParent: 0, description: 'Lorem ipsum ...'},
+  {name: 'Android', categoryIdParent: 0, description: 'Lorem ipsum ...'},
+  {name: 'C', categoryIdParent: 0, description: 'Lorem ipsum ...'},
+  {name: 'C#', categoryIdParent: 0, description: 'Lorem ipsum ...'},
+  {name: 'C++', categoryIdParent: 0, description: 'Lorem ipsum ...'},
+  {name: 'Delphi/Object Pascal', categoryIdParent: 0, description: 'Lorem ipsum ...'},
+  {name: 'GO', categoryIdParent: 0, description: 'Lorem ipsum ...'},
+  {name: 'HTML/CSS', categoryIdParent: 0, description: 'Lorem ipsum ...'},
+  {name: 'IOS', categoryIdParent: 0, description: 'Lorem ipsum ...'},
+  {name: 'Java', categoryIdParent: 0, description: 'Lorem ipsum ...'},
+  {name: 'Javascript', categoryIdParent: 0, description: 'Lorem ipsum ...'},
+  {name: 'Matlab', categoryIdParent: 0, description: 'Lorem ipsum ...'},
+  {name: 'Objective-C', categoryIdParent: 0, description: 'Lorem ipsum ...'},
+  {name: 'PHP', categoryIdParent: 0, description: 'Lorem ipsum ...'},
+])
