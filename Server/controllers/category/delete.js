6961dafdf86e9a5e4b806c516e4c69e68a8abb5d
Maciej Małaszek <malasz95@gmail.com>
2017-06-29 21:35:54 +0200

    Poprawione usuwanie category. Sprawdzana obecność ogłoszeń powiązanych z kategorią.

--- Server/controllers/category/delete.js
+++ Server/controllers/category/delete.js
@@ -2,21 +2,28 @@
  * Created by malasz on 4/24/17.
  */
 var Category = require('./../../models/category.js');
-
+var Ad = require('./../../models/ad.js');
 
 module.exports.main = function (request, response) {
     Category.findOne({
         where:  {categoryIdParent: request.params.categoryId},
         attributes: {exclude: ['id','createdAt', 'updatedAt'], include:['categoryId']}
     }).then(function (category) {
-        if(category!==null)
-            response.status(406).json({messages:'Nie udało się usunąć kategorii', categoryId:"Nie znaleziono kategorii o podanym ID!"})
+        if(category===null)
+            response.status(406).json({messages:'Nie udało się usunąć kategorii', categoryId:"Nie znaleziono kategorii o podanym ID!"});
         else {
-            Category.destroy({where: {categoryID: request.params.categoryId}}).then(function () {
-                response.status(204).json({messages:'Pomyślnie usunięto kategorię'});
-            })
+            Ad.findOne({
+                where: {categoryId: category.categoryId },
+                attributes: {exclude: ['id', 'createdAt', 'updatedAt']}
+            }).then(function (ad) {
+                if(ad === null){
+                    Category.destroy({where: {categoryID: request.params.categoryId}}).then(function () {
+                    response.status(200).json({messages:'Pomyślnie usunięto kategorię'});
+                });}
+                else{
+                    response.status(406).json({messages:'Nie udało się usunąć kategorii', categoryId: "Istnieją ogłoszenia powiązane z tą kategorią!"});
+                }
+            });
         }
     })
-
-
 }
