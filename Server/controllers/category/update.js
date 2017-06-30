4291a681b605db18ae472ad08ea99d3e2f1a7d27
Maciej Małaszek <malasz95@gmail.com>
2017-06-30 20:52:09 +0200

    Dodano sprawdzanie unikatowości nazwy kategorii w aktualizacji. Przyjęto, że jeśli brak parentId, to automatycznie nadaje wartość 0 (bez różnicy w płaskiej liście).

--- Server/controllers/category/update.js
+++ Server/controllers/category/update.js
@@ -10,47 +10,55 @@ module.exports.main = function (request, response) {
         where:  {categoryId: request.params.categoryId},
         attributes: {exclude: ['id','createdAt', 'updatedAt'], include:['categoryId']}
     }).then( function (category) {
-            if(category===null)
-                response.status(404).json({messages:'Nie udało się zaktualizować kategorii', categoryId:"Nie znaleziono kategorii o podanym ID!"})
-            Category.findAll({
-                where:  {categoryId: request.body.categoryIdParent},
-                attributes: {exclude: ['id','createdAt', 'updatedAt'], include:['categoryId']}
-            }).then( function (category) {
-                if ((category.length === 0 && request.body.categoryIdParent !== 0) || request.body.categoryIdParent < 0)
-                    response.status(406).json({messages:'Nie udało się zaktualizować kategorii', categoryIdParent:"Id rodzica musi zostać zdefiniowane i być równe przynajmniej 0!"})
-                else {
-                    if (typeof request.body.name === 'undefined' || !request.body.name)
-                        response.status(406).json({
-                            messages: 'Nie udało się dodać kategorii!',
-                            name: 'Nazwa kategorii musi zostać podana.'
-                        })
-                    else {
-                        if (typeof request.body.description === 'undefined' || !request.body.description)
-                            response.status(406).json({messages:'Nie udało się dodać kategorii!',description:'Opis kategorii musi zostać podany'})
-                        else {
-                            Category.update({
-                                    categoryIdParent: request.body.categoryIdParent,
-                                    name: request.body.name,
-                                    description: request.body.description
-                                },
-                                {
-                                    where: {categoryId: request.params.categoryId}
-                                }).then(function (category) {
-                                response.status(204).json({messages:"Kategoria została pomyślnie zaktualizowana"})
-                            })
+             if(category===null)
+                 response.status(404).json({messages:'Nie udało się zaktualizować kategorii', categoryId:"Nie znaleziono kategorii o podanym ID!"})
+        else {
+                 parentId=request.body.categoryIdParent;
+                 if((category.length === 0 && request.body.categoryIdParent !== 0) || request.body.categoryIdParent < 0 || request.body.categoryIdParent===null)
+                     parentId = 0;
+                 Category.findAll({
+                         where:  {categoryId: parentId},
+                         attributes: {exclude: ['id','createdAt', 'updatedAt'], include:['categoryId']}
+                     }).then( function (parent) {
+                        if(parentId == 0 || parent !== null)
+                        {
+                            if (typeof request.body.name === 'undefined' || !request.body.name)
+                                response.status(406).json({ messages: 'Nie udało się zaktualizować kategorii!',
+                                                            name: 'Nazwa kategorii musi zostać podana.'})
+                            else {
+                                if (typeof request.body.description === 'undefined' || !request.body.description)
+                                    response.status(406).json({messages:'Nie udało się zaktualizować kategorii!',description:'Opis kategorii musi zostać podany'})
+                                else {
+                                    Category.findAll({ where:  {name: request.body.name},
+                                        attributes: {exclude: ['id','createdAt', 'updatedAt'], include:['categoryId']}})
+                                        .then(function (categories) {
+                                            if(categories.length === 0 || (categories.length === 1 && categories[0].categoryId==request.params.categoryId) )
+                                            {
+                                                Category.update({
+                                                        categoryIdParent: request.body.categoryIdParent,
+                                                        name: request.body.name,
+                                                        description: request.body.description
+                                                    },
+                                                    {
+                                                        where: {categoryId: request.params.categoryId}
+                                                    }).then(function (category) {
+                                                response.status(200).json({messages:"Kategoria została pomyślnie zaktualizowana"})
+                                                })
+                                            }
+                                            else {
+                                                response.status(406).json({messages:'Nie udało się zaktualizować kategorii!',name:'Nazwa musi być unikatowa!'})
+                                            }
+
+                                        });
+                                }
+                            }
+                     }
+                     else {
+                            response.status(406).json({messages:'Nie udało się zaktualizować kategorii!',categoryIdParent:'Rodzic musi istnieć!'})
                         }
+                     });
+             }
 
-                    }
-
-                }
-
-            })
-
-        },
-        function (errors) {
-            response.status(406).json({messages:'DB ERROR', info:errors.toString()})
-        })
-
-
+    });
 
 }
